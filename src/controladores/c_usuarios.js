692744582f2355fe9edf6cdf256ea336bc2c55b3
carrebola <carrebola@fpllefia.com>
2022-05-13 16:55:25 +0200

    leer y borrar usuario ok

--- src/controladores/c_usuarios.js
+++ src/controladores/c_usuarios.js
@@ -15,8 +15,18 @@ module.exports = {
 
         res.send('leer usuario')
     },
-    leerUsuarioId : (req, res)=>{
-        res.send('Estás en la leerUsuarioId')
+    leerUsuarioId : async (req, res)=>{
+        //leer de la base de datos un elemento
+        try {
+            const id = req.params.id
+            const usuarioEncontrado = await m_usuarios.findById(id)
+            res.json(usuarioEncontrado)
+        } catch (error) {
+            res.json({
+                error: error,
+                mensaje: 'Ohhhhh, algo ha ido mal en la petición con la bd ' + error
+            })
+        }       
     },
     crearUsuario : (req, res)=>{
         res.send('Estás en  crearUsuario')
@@ -24,7 +34,18 @@ module.exports = {
     editarUsuarioId : (req, res)=>{
         res.send('Estás en  editarUsuarioId')
     },
-    borrarUsuarioId : (req, res)=>{
-        res.send('Estás en  borrarUsuarioId')
+    borrarUsuarioId : async (req, res)=>{
+        //borrar de la base de datos un elemento
+        try {
+            const id = req.params.id
+            const usuarioEncontrado = await m_usuarios.findByIdAndDelete(id)
+            res.json(usuarioEncontrado)
+        } catch (error) {
+            res.json({
+                error: error,
+                mensaje: 'Ohhhhh, algo ha ido mal en la petición con la bd ' + error
+            })
+        }
+
     }
 }
